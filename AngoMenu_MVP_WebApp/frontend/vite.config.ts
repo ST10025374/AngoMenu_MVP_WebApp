f771b8a8f87dd39bb0cd3999de3780f103fbc55b
Miguel Almeida <104522857+ST10025374@users.noreply.github.com>
2026-03-04 12:47:24 +0100

    added Vite.Config

--- AngoMenu_MVP_WebApp/frontend/vite.config.ts
+++ AngoMenu_MVP_WebApp/frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:7210', // since you're running backend on 7210
+                changeOrigin: true,
+                secure: false, // dev cert
+            },
+        },
+    },
 })
