c17b28d40a57d31c2d7a25fbb530786e56d90702
Miguel Almeida <104522857+ST10025374@users.noreply.github.com>
2026-03-23 18:49:04 +0100

    Made Migration

---
+++ AngoMenu_MVP_WebApp/Data/Migrations/20260323174430_ThirdroleManagerAdded.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngoMenu_MVP_WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdroleManagerAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Restaurants",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerId",
+                table: "Restaurants",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Municipality",
+                table: "Restaurants",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Neighborhood",
+                table: "Restaurants",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Restaurants",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetName",
+                table: "Restaurants",
+                type: "nvarchar(180)",
+                maxLength: 180,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_ManagerId",
+                table: "Restaurants",
+                column: "ManagerId",
+                unique: true,
+                filter: "[ManagerId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Restaurants_Users_ManagerId",
+                table: "Restaurants",
+                column: "ManagerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Restaurants_Users_ManagerId",
+                table: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Restaurants_ManagerId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Municipality",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Neighborhood",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "StreetName",
+                table: "Restaurants");
+        }
+    }
+}
